387d16201e619251da901c9e9a3b493c8e451498
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-04-04 16:02:19 -0700

    move glow from leaves to overlay

--- data.lua
+++ data.lua
@@ -39,37 +39,6 @@ local glow_animation_glow = {
 for _, tree in pairs(data.raw.tree) do
     if tree.variations then
         for _, variation in pairs(tree.variations) do
-            -- if variation.leaves then
-            --     local animation = variation.leaves
-            --     if animation.layers then
-            --         -- local original_layers = util.table.deepcopy(animation.layers)
-            --         -- local new_layer = util.table.deepcopy(animation.layers)
-            --         -- new_layer.
-            --         -- animation.layers = {
-            --         --     original_layers,
-            --         --     new_layer
-            --         -- }
-            --     else
-            --         local original_animation = util.table.deepcopy(animation)
-            --         local glow_layer = util.table.deepcopy(animation)
-            --         local light_layer = util.table.deepcopy(animation)
-            --         if not (glow_layer.draw_as_shadow or glow_layer.draw_as_glow or glow_layer.draw_as_light) then
-            --             glow_layer.draw_as_glow = true
-            --             light_layer.draw_as_light = true
-            --             -- explosion_animation[1].frame_count = original_animation.frame_count
-            --             -- glow_animation.frame_count = original_animation.frame_count
-            --             variation.leaves.layers = {
-            --                 original_animation,
-            --                 -- light_layer,
-            --                 -- glow_layer
-            --                 -- explosion_animation[1]
-            --                 glow_animation_glow,
-            --                 -- glow_animation_light,
-            --             }
-            --         end
-            --         log(serpent.block(tree.variations[_].leaves))
-            --     end
-            -- end
             if variation.overlay then
                 local animation = util.table.deepcopy(variation.overlay)
                 variation.overlay = {
